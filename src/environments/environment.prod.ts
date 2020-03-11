6e838a4ec208fc6b8f0a45958f6b0c56f4cbe777
TomTolleson <tomtolleson@gmail.com>
2020-03-11 10:53:35 -0400

    starting from tutorial

--- src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-import { versions } from './versions'
-import * as pkg from '../../package.json'
-
-export const environment = {
-  production: true,
-  version: pkg.version,
-  gitHash: versions.revision,
-  butterCMSToken: '10de8a1782f01676902398495c4062893956ac9c'
-}
